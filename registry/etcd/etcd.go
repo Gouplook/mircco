7309ff29b1b126e90b4c684d673c3056724b9372
yinjinlin <Gouplook@163.com>
2020-09-12 14:57:54 +0800

    服务注册修改

--- registry/etcd/etcd.go
+++ registry/etcd/etcd.go
@@ -2,16 +2,16 @@ package etcd
 
 import (
 	"context"
+	"fmt"
 	"github.com/coreos/etcd/clientv3"
 	"koalamicrro/registry"
 )
 
 //etcd 注册插件
 type EtcdRegistry struct {
-
-	options *registry.Options
-	client *clientv3.Client
-	serviceCh chan * registry.Service
+	options   *registry.Options
+	client    *clientv3.Client
+	serviceCh chan *registry.Service
 }
 
 type AllServiceInfo struct {
@@ -19,28 +19,29 @@ type AllServiceInfo struct {
 }
 
 type RegisterServie struct {
-	id clientv3.LeaseID
-	service *registry.Service
+	id         clientv3.LeaseID
+	service    *registry.Service
 	registered bool
 	keeAliveCh <-chan *clientv3.LeaseKeepAliveResponse
-
 }
 
 //插件的名字
 
-func (e *EtcdRegistry)Name()string{
+func (e *EtcdRegistry) Name() string {
 	return "etcd"
 
 }
-func (e *EtcdRegistry)Init(ctx context.Context,opts ...registry.Option)(err error){
+func (e *EtcdRegistry) Init(ctx context.Context, opts ...registry.Option) (err error) {
 
 	return
 }
 
 //服务注册
 func (e *EtcdRegistry) Register(ctx context.Context, service *registry.Service) (err error) {
+	fmt.Println("服务注册")
 	return
 }
+
 //服务反注册
 func (e *EtcdRegistry) Unregister(ctx context.Context, service *registry.Service) (err error) {
 	return
